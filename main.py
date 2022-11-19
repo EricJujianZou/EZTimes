be7e64dda1348ec777f2a52200caa4cf2ef8f982
Carlton Qian <69259864+get-oofed-nerd@users.noreply.github.com>
2022-11-19 14:23:14 -0500

    Pygame window
    setting things up

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("init")
+import pygame
+import threading
+
+#initializing the screen
+pygame.init()
+screen = pygame.display.set_mode(size = (2560, 1400), flags = pygame.FULLSCREEN)
+
+#intializing the acquisition of inputs
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                #exit the program if the escape key is pressed
+                pygame.quit()
